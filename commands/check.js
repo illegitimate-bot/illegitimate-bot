4889ac1808016add07af67f6d702c56794de440b
Taken <taken@mairimashita.org>
2023-03-14 19:12:18 +0100

    Updating check command

--- commands/check.js
+++ commands/check.js
@@ -23,11 +23,15 @@ module.exports = {
         const ign = interaction.options.getString('ign');
         const mojang = "https://api.mojang.com/users/profiles/minecraft/"
         const slothPixel = "https://api.slothpixel.me/api/players/";
+        const guildAPI = "https://api.slothpixel.me/api/guilds/"
         const minotar = "https://minotar.net/helm/";
         const embedColor = Number(color.replace("#", "0x"));
 
         const userCheck = await fetch(mojang + ign);
-        const stats = await fetch(slothPixel + ign);
+        const userUUID = userCheck.data.id;
+
+        const stats = await fetch(slothPixel + userUUID);
+        const guildCheck = await fetch(guildAPI + userUUID);
         const head = minotar + ign;
         
         if (!ign) {
@@ -48,30 +52,34 @@ module.exports = {
         await interaction.reply({
             embeds: [{
                 title: stats.data.username,
-                description: "**Stats:**",
+                description: "**Network Level:** `" + stats.data.level.toString() + "`\n" + 
+                "**Current Guild:** `" +  guildCheck.data.name + "`",
                 color: embedColor,
                 thumbnail: {
                     url: head
                 },
                 footer: {
-                    text: "Developed by @Taken#0002"
+                    text: interaction.guild.name + " | Developed by @Taken#0002",
+                    icon_url: interaction.guild.iconURL()
                 },
                 fields: [
                     {
-                        name: "**Network Level**",
-                        value: stats.data.level.toString(),
-                    },
-                    {
                         name: "**Bedwars**",
-                        value: "**Stars:** " + stats.data.stats.BedWars.level.toString() + "\n" +
-                        "**FKDR:** " + stats.data.stats.BedWars.final_k_d.toString() + "\n" +
-                        "**Wins:** " + stats.data.stats.BedWars.wins.toString()
+                        value: "**• Stars:** `" + stats.data.stats.BedWars.level.toString() + "`\n" +
+                        "**• FKDR:** `" + stats.data.stats.BedWars.final_k_d.toString() + "`\n" +
+                        "**• Wins:** `" + stats.data.stats.BedWars.wins.toString() + "`"
                     },
                     {
                         name: "**Skywars**",
-                        value: "**Stars:** " + stats.data.stats.SkyWars.level.toFixed(2).toString() + "\n" +
-                        "**KDR**: " + stats.data.stats.SkyWars.kill_death_ratio.toString() + "\n" +
-                        "**Wins:** " + stats.data.stats.SkyWars.wins.toString()
+                        value: "**• Stars:** `" + stats.data.stats.SkyWars.level.toFixed(2).toString() + "`\n" +
+                        "**• KDR:** `" + stats.data.stats.SkyWars.kill_death_ratio.toString() + "`\n" +
+                        "**• Wins:** `" + stats.data.stats.SkyWars.wins.toString() + "`"
+                    },
+                    {
+                        name: "**Duels**",
+                        value: "**• Wins:** `" + stats.data.stats.Duels.general.wins.toString() + "`\n" + 
+                        "**• KDR:** `" + stats.data.stats.Duels.general.kd_ratio.toFixed(2).toString() + "`\n" + 
+                        "**• WLR:** `" + stats.data.stats.Duels.general.win_loss_ratio.toFixed(2).toString() + "`"
                     }
                 ]
             }]

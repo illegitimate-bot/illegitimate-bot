3d0c21301f5e9a0f4a1a00a337416a48fe7fb27d
Taken <taken@mairimashita.org>
2023-03-19 15:01:17 +0100

    Adding one liner to set the guild id

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,8 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const { clientId, realguildId, testguildId, token } = require('./config.json');
 const fs = require('node:fs');
+
+const guildId = testguildId;
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier

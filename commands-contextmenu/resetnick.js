0a4181f583e9ba47323162696ce4287692f36101
Taken <taken@mairimashita.org>
2023-04-18 17:25:17 +0200

    Added first context menu commands

---
+++ commands-contextmenu/resetnick.js
@@ -0,0 +1,34 @@
+const { ContextMenuCommandBuilder, ApplicationCommandType, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    name: 'Reset Nickname',
+    description: 'Reset your nickname.',
+    type: 'contextmenu',
+
+    data: new ContextMenuCommandBuilder()
+        .setName('resetnick')
+        .setType(ApplicationCommandType.User)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageNicknames),
+
+    async execute(interaction) {
+
+        const { targetId } = interaction
+        const target = await interaction.guild.members.fetch(targetId);
+
+        if (!target) {
+            return interaction.reply({ content: 'That user does not exist.', ephemeral: true });
+        }
+
+        if (target.id === interaction.user.id) {
+            return interaction.reply({ content: 'You can\'t reset your own nickname.', ephemeral: true });
+        }
+
+        if (!target.manageable) {
+            return interaction.reply({ content: 'I cannot reset that user\'s nickname.', ephemeral: true });
+        }
+
+        await target.setNickname(target.user.username, 'Reset by ' + interaction.user.username + "#" + interaction.user.discriminator);
+
+        return interaction.reply({ content: `Reset ${target.user.username}'s nickname.`, ephemeral: true });
+    }
+};
--- index.js
+++ index.js
@@ -77,6 +77,41 @@ client.on(Events.InteractionCreate, async interaction => {
     }
 });
 
+//! commands
+const contextMenuPath = path.join(__dirname, 'commands-contextmenu');
+const contextMenuFiles = fs.readdirSync(contextMenuPath).filter(file => file.endsWith('.js'));
+
+for (const file of contextMenuFiles) {
+
+    const filePath = path.join(contextMenuPath, file);
+    const cmd = require(filePath);
+
+    if ('data' in cmd && 'execute' in cmd && cmd.type === 'contextmenu') {
+        client.commands.set(cmd.data.name, cmd);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data", "execute" or "type" property.`);
+    }
+}
+
+//! context menu command handler
+client.on(Events.InteractionCreate, async interaction => {
+    if(!interaction.isContextMenuCommand()) return;
+    
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+    }
+});
+
 //! button events
 const btnPath = path.join(__dirname, 'events', 'buttons');
 const btnFiles = fs.readdirSync(btnPath).filter(file => file.endsWith('.js'));

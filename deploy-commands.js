8e5c4af4b6cd50009ff432803e3b2d0b92fdfda4
Taken <taken@mairimashita.org>
2023-04-18 22:53:46 +0200

    Updating to script args

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,36 +3,109 @@ const env = require('dotenv').config();
 const token = process.env.TOKEN;
 const clientId = process.env.CLIENTID;
 const fs = require('node:fs');
+const args = process.argv.slice(2);
+const arg = args[0];
 
-
-const commands = [];
-// Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+if (!arg) {
+	console.log('Please specify a command to run!');
 }
+else if (arg === '--prod') {	
+	const commands = [];
+	// Grab all the command files from the commands directory you created earlier
+	const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(token);
-
-
-// and deploy your commands!
-(async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
-
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: commands },
-		);
-
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const command = require(`./commands/${file}`);
+		commands.push(command.data.toJSON());
 	}
-})();
+
+	// Construct and prepare an instance of the REST module
+	const rest = new REST({ version: '10' }).setToken(token);
+
+
+	// and deploy your commands!
+	(async () => {
+		try {
+			console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+			// The put method is used to fully refresh all commands in the guild with the current set
+			const data = await rest.put(
+				Routes.applicationCommands(clientId),
+				{ body: commands },
+			);
+
+			console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		} catch (error) {
+			// And of course, make sure you catch and log any errors!
+			console.error(error);
+		}
+	})();
+}
+else if (arg === '--dev') {
+	const commands = [];
+	// Grab all the command files from the commands directory you created earlier
+	const commandFiles = fs.readdirSync('./commands-contextmenu').filter(file => file.endsWith('.js'));
+
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const command = require(`./commands-contextmenu/${file}`);
+		commands.push(command.data.toJSON());
+	}
+
+	// Construct and prepare an instance of the REST module
+	const rest = new REST({ version: '10' }).setToken(token);
+
+
+	// and deploy your commands!
+	(async () => {
+		try {
+			console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+			// The put method is used to fully refresh all commands in the guild with the current set
+			const data = await rest.put(
+				Routes.applicationCommands(clientId),
+				{ body: commands },
+			);
+
+			console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		} catch (error) {
+			// And of course, make sure you catch and log any errors!
+			console.error(error);
+		}
+	})();
+} else if (arg === '--contextmenu') {
+	const commands = [];
+	// Grab all the command files from the commands directory you created earlier
+	const commandFiles = fs.readdirSync('./commands-contextmenu').filter(file => file.endsWith('.js'));
+
+	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	for (const file of commandFiles) {
+		const command = require(`./commands-contextmenu/${file}`);
+		commands.push(command.data.toJSON());
+	}
+
+	// Construct and prepare an instance of the REST module
+	const rest = new REST({ version: '10' }).setToken(token);
+
+
+	// and deploy your commands!
+	(async () => {
+		try {
+			console.log(`Started refreshing ${commands.length} application contextmenu (/) commands.`);
+
+			// The put method is used to fully refresh all commands in the guild with the current set
+			const data = await rest.put(
+				Routes.applicationCommands(clientId),
+				{ body: commands },
+			);
+
+			console.log(`Successfully reloaded ${data.length} application contextmenu (/) commands.`);
+		} catch (error) {
+			// And of course, make sure you catch and log any errors!
+			console.error(error);
+		}
+	})();
+} else if (arg && arg !== '--prod' && arg !== '--dev' && arg !== '--contextmenu') {
+	console.log('Invalid argument!');
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+const args = process.argv.slice(2);
+
+const arg = args[0];
+
+console.log(arg);

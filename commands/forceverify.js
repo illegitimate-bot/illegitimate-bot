f791749225450aeee4199e572e7f0996f5ca10f8
Taken <taken@mairimashita.org>
2023-06-20 21:35:08 +0200

    Added support for new usernames

--- commands/forceverify.js
+++ commands/forceverify.js
@@ -33,15 +33,26 @@ module.exports = {
 
         const user1 = interaction.options.getUser('user');
         const user = interaction.guild.members.cache.get(user1.id);
-        const fullUsername = user1.username + "#" + user1.discriminator
         const ign = interaction.options.getString('ign');
-        const mod = interaction.user.username + "#" + interaction.user.discriminator
+        const mod = interaction.user
 
         const mojang = "https://api.mojang.com/users/profiles/minecraft/"
         const slothPixel = "https://api.slothpixel.me/api/players/";
         const guildAPI = "https://api.slothpixel.me/api/guilds/"
         const minotar = "https://minotar.net/helm/";
         const embedColor = Number(color.replace("#", "0x"));
+
+        if (user1.discriminator == "0") {
+            var username = user1.username
+        } else {
+            var username = user1.username + "#" + user.discriminator
+        }
+
+        if (mod.discriminator == "0") {
+            var modName = mod.username
+        } else {
+            var modName = mod.username + "#" + mod.discriminator
+        }
 
         if (!user) {
             interaction.editReply('Please provide a user to force verify.')
@@ -67,10 +78,10 @@ module.exports = {
             return
         }
 
-				const userCheck = await fetch(mojang + ign);
-				const userUUID = userCheck.data.id;
+        const userCheck = await fetch(mojang + ign);
+        const userUUID = userCheck.data.id;
 
-				try {
+        try {
             await fetch(slothPixel + userUUID);
         } catch (err) {
             interaction.editReply('That player doesn\'t exist. [Hypixel]')
@@ -89,12 +100,12 @@ module.exports = {
 
         if (responseGuildID !== hypixelGuildID) {
 
-            await user.roles.add(defaultMember, "User was force verified by " + mod);
+            await user.roles.add(defaultMember, "User was force verified by " + modName);
 
             await interaction.editReply({
                 embeds: [{
                     title: interaction.guild.name,
-                    description: "You have successfully force verified `" + fullUsername + "` with the account `" + hypixelCheck.data.username + "`.",
+                    description: "You have successfully force verified `" + username + "` with the account `" + hypixelCheck.data.username + "`.",
                     color: embedColor,
                     thumbnail: {
                         url: head
@@ -113,40 +124,40 @@ module.exports = {
         const guildRank = GuildMembers.find(member => member.uuid === hypixelCheck.data.uuid).rank;
 
         if (guildRank === "Guild Master" && guildCheck.data.id === hypixelGuildID) {
-            await user.roles.add(gm, "User was force verified by " + mod);
-            await user.roles.add(guildRole, "User was force verified by " + mod)
-            await user.roles.add(guildStaff, "User was force verified by " + mod)
+            await user.roles.add(gm, "User was force verified by " + modName);
+            await user.roles.add(guildRole, "User was force verified by " + modName)
+            await user.roles.add(guildStaff, "User was force verified by " + modName)
         }
 
         if (guildRank === "Manager" && guildCheck.data.id === hypixelGuildID) {
-            await user.roles.add(manager, "User was force verified by " + mod);
-            await user.roles.add(guildRole, "User was force verified by " + mod)
-            await user.roles.add(guildStaff, "User was force verified by " + mod)
+            await user.roles.add(manager, "User was force verified by " + modName);
+            await user.roles.add(guildRole, "User was force verified by " + modName)
+            await user.roles.add(guildStaff, "User was force verified by " + modName)
         }
 
         if (guildRank === "Moderator" && guildCheck.data.id === hypixelGuildID) {
-            await user.roles.add(moderator, "User was force verified by " + mod);
-            await user.roles.add(guildRole, "User was force verified by " + mod)
-            await user.roles.add(guildStaff, "User was force verified by " + mod)
+            await user.roles.add(moderator, "User was force verified by " + modName);
+            await user.roles.add(guildRole, "User was force verified by " + modName)
+            await user.roles.add(guildStaff, "User was force verified by " + modName)
         }
         
         if (guildRank === "Beast" && guildCheck.data.id === hypixelGuildID) {
-            await user.roles.add(beast, "User was force verified by " + mod);
-            await user.roles.add(guildRole, "User was force verified by " + mod)
+            await user.roles.add(beast, "User was force verified by " + modName);
+            await user.roles.add(guildRole, "User was force verified by " + modName)
         }
 
         if (guildRank === "Member" && guildCheck.data.id === hypixelGuildID) {
-            await user.roles.add(member, "User was force verified by " + mod);
-            await user.roles.add(guildRole, "User was force verified by " + mod)
+            await user.roles.add(member, "User was force verified by " + modName);
+            await user.roles.add(guildRole, "User was force verified by " + modName)
         }
 
         if (guildRank === "Trial Member" && guildCheck.data.id === hypixelGuildID) {
-            await user.roles.add(trialmember, "User was force verified by " + mod);
-            await user.roles.add(guildRole, "User was force verified by " + mod)
+            await user.roles.add(trialmember, "User was force verified by " + modName);
+            await user.roles.add(guildRole, "User was force verified by " + modName)
         }
 
-        await user.roles.add(defaultMember, "User was force verified by " + mod);
-
+        await user.roles.add(defaultMember, "User was force verified by " + modName);
+        
         const newVerify = new verify({
             _id: new mongoose.Types.ObjectId(),
             userID: user.id,
@@ -158,7 +169,7 @@ module.exports = {
         await interaction.editReply({
             embeds: [{
                 title: interaction.guild.name,
-                description: "You have successfully force verified `" + fullUsername + "` with the account `" + hypixelCheck.data.username + "`.",
+                description: "You have successfully force verified `" + username + "` with the account `" + hypixelCheck.data.username + "`.",
                 color: embedColor,
                 thumbnail: {
                     url: head

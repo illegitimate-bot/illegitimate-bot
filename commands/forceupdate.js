a601305f644fa56ca450a19ad2f88e190eb3592d
Taken <taken@mairimashita.org>
2023-08-13 00:49:51 +0200

    Updating forceupdate and fupdate

--- commands/forceupdate.js
+++ commands/forceupdate.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder, PermissionFlagsBits, userMention } = require('discord.js');
+const env = require('dotenv').config();
+const { hypixelAPIKey } = process.env.HYPIXELAPIKEY;
 const { hypixelGuildID, color } = require('../config/options.json');
 const { gm, manager, moderator, beast, elite, member, trialmember, guildRole, guildStaff, defaultMember } = require('../config/roles.json');
 const verify = require('../schemas/verifySchema.js')
@@ -27,264 +29,260 @@ module.exports = {
 
         const user = interaction.options.getUser('user');
         const usermentioned = userMention(user.id);
-        const guild = interaction.guild;
         const verifyData = await verify.findOne({ userID: user.id })
         const embedColor = Number(color.replace("#", "0x"));
 
-        const user1 = guild.members.cache.get(user.id);
+        const user1 = interaction.guild.members.cache.get(user.id);
         const roleManage = user1.roles;
 
         if (!verifyData) {
-
-            for (let i = 0; i < removeThese.length; i++) {
-                await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
-            }
-
             await interaction.editReply({
                 embeds: [{
-                    description: usermentioned + " was given the the Default Member role.",
+                    description: "You are not verified. Please run `/verify` to verify yourself",
                     color: embedColor,
+                    thumbnail: {
+                        url: head
+                    },
                     footer: {
                         text: interaction.guild.name + " | Developed by @Taken#0002",
                         icon_url: interaction.guild.iconURL({ dynamic: true })
                     }
                 }]
             })
-            await roleManage.add(defaultMember, "User was force updated.")
             return
         }
 
-        const slothPixel = "https://api.slothpixel.me/api/players/";
-        const guildAPI = "https://api.slothpixel.me/api/guilds/"
         const mojangAPI = "https://api.mojang.com/user/profile/"
+		const guildlAPI	= "https://api.hypixel.net/guild"
         const minotar = "https://minotar.net/helm/";
-
+		const guild = guildlAPI + "?key=" + hypixelAPIKey + "&player=" + verifyData.uuid;
         const userCheck = await fetch(mojangAPI + verifyData.uuid);
-        const hypixelCheck = await fetch(slothPixel + verifyData.uuid);
+        const guildCheck = await fetch(guild);
         const head = minotar + userCheck.data.name;
 
-        try {
-            const guildCheck = await fetch(guildAPI + verifyData.uuid);
-            var responseGuildID = guildCheck.data.id;
-        } catch (err) {
-            var responseGuildID = null;
-        }
+		if (!guildCheck.data.guild) {
+			var responseGuildID = null
+		} else {
+			var responseGuildID = guildCheck.data.guild._id
+		}
 
         if (responseGuildID !== hypixelGuildID) {
 
-            for (let i = 0; i < removeThese.length; i++) {
-                await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
-            }
+			for (let i = 0; i < removeThese.length; i++) {
+				await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
+			}
 
-            await interaction.editReply({
-                embeds: [{
-                    description: usermentioned + " was given the the Default Member role.",
-                    color: embedColor,
-                    thumbnail: {
-                        url: head
-                    },
-                    footer: {
-                        text: interaction.guild.name + " | Developed by @Taken#0002",
-                        icon_url: interaction.guild.iconURL({ dynamic: true })
-                    }
-                }]
-            })
-            await roleManage.add(defaultMember)
-            return
-        }
+			await interaction.editReply({
+				embeds: [{
+					description: usermentioned + " was given the the Default Member role.",
+					color: embedColor,
+					thumbnail: {
+						url: head
+					},
+					footer: {
+						text: interaction.guild.name + " | Developed by @Taken#0002",
+						icon_url: interaction.guild.iconURL({ dynamic: true })
+					}
+				}]
+			})
+			await roleManage.add(defaultMember)
+			return
+		}
 
-        const guildCheck = await fetch(guildAPI + verifyData.uuid);
-        const GuildMembers = await guildCheck.data.members;
-        const guildRank = GuildMembers.find(member => member.uuid === verifyData.uuid).rank;
+		if (responseGuildID === hypixelGuildID) {
 
-        if (guildRank === 'Guild Master' && responseGuildID === hypixelGuildID) {
+			const GuildMembers = await guildCheck.data.members;
+			const guildRank = GuildMembers.find(member => member.uuid === verifyData.uuid).rank;
 
-            for (let i = 0; i < removeThese.length; i++) {
-                await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
-            }
-            
-            await roleManage.add(guildRole, "User was force updated.")
-            await roleManage.add(guildStaff, "User was force updated.")
-            await roleManage.add(gm, "User was force updated.")
-            await roleManage.add(defaultMember, "User was force updated.")
+			if (guildRank === 'Guild Master') {
 
-            
-            await interaction.editReply({
-                embeds: [{
-                    description: usermentioned + "'s rank has been updated to `Guild Master`",
-                    color: embedColor,
-                    thumbnail: {
-                        url: head
-                    },
-                    footer: {
-                        text: interaction.guild.name + " | Developed by @Taken#0002",
-                        icon_url: interaction.guild.iconURL({ dynamic: true })
-                    }
-                }]
-            })
-        }
+				for (let i = 0; i < removeThese.length; i++) {
+					await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
+				}
 
-        if (guildRank === 'Manager' && responseGuildID === hypixelGuildID) {
+				await roleManage.add(guildRole, "User was force updated.")
+				await roleManage.add(guildStaff, "User was force updated.")
+				await roleManage.add(gm, "User was force updated.")
+				await roleManage.add(defaultMember, "User was force updated.")
 
-            for (let i = 0; i < removeThese.length; i++) {
-                await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
-            }
-            
-            await roleManage.add(guildRole, "User was force updated.")
-            await roleManage.add(guildStaff, "User was force updated.")
-            await roleManage.add(manager, "User was force updated.")
-            await roleManage.add(defaultMember, "User was force updated.")
 
-            
-            await interaction.editReply({
-                embeds: [{
-                    description: usermentioned + "'s rank has been updated to `Manager`",
-                    color: embedColor,
-                    thumbnail: {
-                        url: head
-                    },
-                    footer: {
-                        text: interaction.guild.name + " | Developed by @Taken#0002",
-                        icon_url: interaction.guild.iconURL({ dynamic: true })
-                    }
-                }]
-            })
-        }
+				await interaction.editReply({
+					embeds: [{
+						description: usermentioned + "'s rank has been updated to `Guild Master`",
+						color: embedColor,
+						thumbnail: {
+							url: head
+						},
+						footer: {
+							text: interaction.guild.name + " | Developed by @Taken#0002",
+							icon_url: interaction.guild.iconURL({ dynamic: true })
+						}
+					}]
+				})
+			}
 
-        if (guildRank === 'Moderator' && responseGuildID === hypixelGuildID) {
+			if (guildRank === 'Manager') {
 
-            for (let i = 0; i < removeThese.length; i++) {
-                await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
-            }
-            
-            await roleManage.add(guildRole, "User was force updated.")
-            await roleManage.add(guildStaff, "User was force updated.")
-            await roleManage.add(moderator, "User was force updated.")
-            await roleManage.add(defaultMember, "User was force updated.")
+				for (let i = 0; i < removeThese.length; i++) {
+					await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
+				}
 
-            
-            await interaction.editReply({
-                embeds: [{
-                    description: usermentioned + "'s rank has been updated to `Moderator`",
-                    color: embedColor,
-                    thumbnail: {
-                        url: head
-                    },
-                    footer: {
-                        text: interaction.guild.name + " | Developed by @Taken#0002",
-                        icon_url: interaction.guild.iconURL({ dynamic: true })
-                    }
-                }]
-            })
+				await roleManage.add(guildRole, "User was force updated.")
+				await roleManage.add(guildStaff, "User was force updated.")
+				await roleManage.add(manager, "User was force updated.")
+				await roleManage.add(defaultMember, "User was force updated.")
 
-        }
 
-        if (guildRank === 'Beast' && responseGuildID === hypixelGuildID) {
+				await interaction.editReply({
+					embeds: [{
+						description: usermentioned + "'s rank has been updated to `Manager`",
+						color: embedColor,
+						thumbnail: {
+							url: head
+						},
+						footer: {
+							text: interaction.guild.name + " | Developed by @Taken#0002",
+							icon_url: interaction.guild.iconURL({ dynamic: true })
+						}
+					}]
+				})
+			}
 
-            for (let i = 0; i < removeThese.length; i++) {
-                await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
-            }
-            
-            await roleManage.add(guildRole, "User was force updated.")
-            await roleManage.add(beast, "User was force updated.")
-            await roleManage.add(defaultMember, "User was force updated.")
+			if (guildRank === 'Moderator') {
 
-            
-            await interaction.editReply({
-                embeds: [{
-                    description: usermentioned + "'s rank has been updated to `Beast`.",
-                    color: embedColor,
-                    thumbnail: {
-                        url: head
-                    },
-                    footer: {
-                        text: interaction.guild.name + " | Developed by @Taken#0002",
-                        icon_url: interaction.guild.iconURL({ dynamic: true })
-                    }
-                }]
-            })
-            return
-        }
+				for (let i = 0; i < removeThese.length; i++) {
+					await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
+				}
 
-        if (guildRank === 'Elite' && responseGuildID === hypixelGuildID) {
+				await roleManage.add(guildRole, "User was force updated.")
+				await roleManage.add(guildStaff, "User was force updated.")
+				await roleManage.add(moderator, "User was force updated.")
+				await roleManage.add(defaultMember, "User was force updated.")
 
-            for (let i = 0; i < removeThese.length; i++) {
-                await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
-            }
-            
-            await roleManage.add(guildRole, "User was force updated.")
-            await roleManage.add(elite, "User was force updated.")
-            await roleManage.add(defaultMember, "User was force updated.")
 
-            
-            await interaction.editReply({
-                embeds: [{
-                    description: usermentioned + "'s rank has been updated to `Elite`.",
-                    color: embedColor,
-                    thumbnail: {
-                        url: head
-                    },
-                    footer: {
-                        text: interaction.guild.name + " | Developed by @Taken#0002",
-                        icon_url: interaction.guild.iconURL({ dynamic: true })
-                    }
-                }]
-            })
-            return
-        }
+				await interaction.editReply({
+					embeds: [{
+						description: usermentioned + "'s rank has been updated to `Moderator`",
+						color: embedColor,
+						thumbnail: {
+							url: head
+						},
+						footer: {
+							text: interaction.guild.name + " | Developed by @Taken#0002",
+							icon_url: interaction.guild.iconURL({ dynamic: true })
+						}
+					}]
+				})
 
-        if (guildRank === 'Member' && responseGuildID === hypixelGuildID) {
+			}
 
-            for (let i = 0; i < removeThese.length; i++) {
-                await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
-            }
-            
-            await roleManage.add(guildRole, "User was force updated.")
-            await roleManage.add(member, "User was force updated.")
-            await roleManage.add(defaultMember, "User was force updated.")
+			if (guildRank === 'Beast') {
 
-            
-            await interaction.editReply({
-                embeds: [{
-                    description: usermentioned + "'s rank has been updated to `Member`.",
-                    color: embedColor,
-                    thumbnail: {
-                        url: head
-                    },
-                    footer: {
-                        text: interaction.guild.name + " | Developed by @Taken#0002",
-                        icon_url: interaction.guild.iconURL({ dynamic: true })
-                    }
-                }]
-            })
-            return
-        }
+				for (let i = 0; i < removeThese.length; i++) {
+					await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
+				}
 
-        if (guildRank === 'Trial Member' && responseGuildID === hypixelGuildID) {
+				await roleManage.add(guildRole, "User was force updated.")
+				await roleManage.add(beast, "User was force updated.")
+				await roleManage.add(defaultMember, "User was force updated.")
 
-            for (let i = 0; i < removeThese.length; i++) {
-                await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
-            }
-            
-            await roleManage.add(guildRole, "User was force updated.")
-            await roleManage.add(trialmember, "User was force updated.")
-            await roleManage.add(defaultMember, "User was force updated.")
 
-            
-            await interaction.editReply({
-                embeds: [{
-                    description: usermentioned + "'s rank has been updated to `Trial Member`.",
-                    color: embedColor,
-                    thumbnail: {
-                        url: head
-                    },
-                    footer: {
-                        text: interaction.guild.name + " | Developed by @Taken#0002",
-                        icon_url: interaction.guild.iconURL({ dynamic: true })
-                    }
-                }]
-            })
-            return
-        }
+				await interaction.editReply({
+					embeds: [{
+						description: usermentioned + "'s rank has been updated to `Beast`.",
+						color: embedColor,
+						thumbnail: {
+							url: head
+						},
+						footer: {
+							text: interaction.guild.name + " | Developed by @Taken#0002",
+							icon_url: interaction.guild.iconURL({ dynamic: true })
+						}
+					}]
+				})
+				return
+			}
+
+			if (guildRank === 'Elite') {
+
+				for (let i = 0; i < removeThese.length; i++) {
+					await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
+				}
+
+				await roleManage.add(guildRole, "User was force updated.")
+				await roleManage.add(elite, "User was force updated.")
+				await roleManage.add(defaultMember, "User was force updated.")
+
+
+				await interaction.editReply({
+					embeds: [{
+						description: usermentioned + "'s rank has been updated to `Elite`.",
+						color: embedColor,
+						thumbnail: {
+							url: head
+						},
+						footer: {
+							text: interaction.guild.name + " | Developed by @Taken#0002",
+							icon_url: interaction.guild.iconURL({ dynamic: true })
+						}
+					}]
+				})
+				return
+			}
+
+			if (guildRank === 'Member') {
+
+				for (let i = 0; i < removeThese.length; i++) {
+					await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
+				}
+
+				await roleManage.add(guildRole, "User was force updated.")
+				await roleManage.add(member, "User was force updated.")
+				await roleManage.add(defaultMember, "User was force updated.")
+
+
+				await interaction.editReply({
+					embeds: [{
+						description: usermentioned + "'s rank has been updated to `Member`.",
+						color: embedColor,
+						thumbnail: {
+							url: head
+						},
+						footer: {
+							text: interaction.guild.name + " | Developed by @Taken#0002",
+							icon_url: interaction.guild.iconURL({ dynamic: true })
+						}
+					}]
+				})
+				return
+			}
+
+			if (guildRank === 'Trial Member') {
+
+				for (let i = 0; i < removeThese.length; i++) {
+					await roleManage.remove(removeThese[i], "Auto role removal. (Force Update)")
+				}
+
+				await roleManage.add(guildRole, "User was force updated.")
+				await roleManage.add(trialmember, "User was force updated.")
+				await roleManage.add(defaultMember, "User was force updated.")
+
+
+				await interaction.editReply({
+					embeds: [{
+						description: usermentioned + "'s rank has been updated to `Trial Member`.",
+						color: embedColor,
+						thumbnail: {
+							url: head
+						},
+						footer: {
+							text: interaction.guild.name + " | Developed by @Taken#0002",
+							icon_url: interaction.guild.iconURL({ dynamic: true })
+						}
+					}]
+				})
+				return
+			}
+		}	
     }
 }

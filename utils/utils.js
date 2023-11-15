325d310de5e4d91a7019daa7bd4e3eba7100f146
Taken <taken@mairimashita.org>
2023-11-15 19:08:13 +0100

    Updated and cleaned up files

---
+++ utils/utils.js
@@ -0,0 +1,5 @@
+const { skywarsLevel } = require('./functions/skywars.js')
+const { bedwarsLevel } = require('./functions/bedwars.js')
+const { hypixelLevel } = require('./functions/hypixel.js')
+
+module.exports = { skywarsLevel, bedwarsLevel, hypixelLevel }

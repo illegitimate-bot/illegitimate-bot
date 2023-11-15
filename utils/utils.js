b45fc07614d2f3307238db64701296a76e3b929d
Taken <taken@mairimashita.org>
2023-11-16 00:38:11 +0100

    Added function for calculating guild exp

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,11 @@
 const { skywarsLevel } = require('./functions/skywars.js')
 const { bedwarsLevel } = require('./functions/bedwars.js')
 const { hypixelLevel } = require('./functions/hypixel.js')
+const { guildLevel } = require('./functions/guild.js')
 
-module.exports = { skywarsLevel, bedwarsLevel, hypixelLevel }
+module.exports = {
+    skywarsLevel,
+    bedwarsLevel,
+    hypixelLevel,
+    guildLevel
+}

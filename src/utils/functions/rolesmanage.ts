e989d0067b62fae168750ca9ed002ceb45deed7d
Taken <taken@mairimashita.org>
2024-01-29 22:04:38 +0100

    Added new role and updated role manager script

--- src/utils/functions/rolesmanage.ts
+++ src/utils/functions/rolesmanage.ts
@@ -7,7 +7,8 @@ import {
     member,
     guildStaff,
     guildRole,
-    defaultMember
+    defaultMember,
+    verifyTick
 } from "config/roles.json"
 const roles = [
     gm,
@@ -33,48 +34,49 @@ type RoleType =
 export default function roleManage(role: RoleType): { rolesToRemove: string[], rolesToAdd: string[] } {
     if (role === "gm") {
         const rolesToRemove = roles.filter(role => role !== gm && role !== guildStaff && role !== guildRole)
-        const rolesToAdd = [gm, guildStaff, guildRole]
+        const rolesToAdd = [gm, guildStaff, guildRole, verifyTick]
         return { rolesToRemove, rolesToAdd }
     }
 
     if (role === "manager") {
         const rolesToRemove = roles.filter(role => role !== manager && role !== guildStaff && role !== guildRole)
-        const rolesToAdd = [manager, guildStaff, guildRole]
+        const rolesToAdd = [manager, guildStaff, guildRole, verifyTick]
         return { rolesToRemove, rolesToAdd }
     }
 
     if (role === "moderator") {
         const rolesToRemove = roles.filter(role => role !== moderator && role !== guildStaff && role !== guildRole)
-        const rolesToAdd = [moderator, guildStaff, guildRole]
+        const rolesToAdd = [moderator, guildStaff, guildRole, verifyTick]
         return { rolesToRemove, rolesToAdd }
     }
 
     if (role === "beast") {
         const rolesToRemove = roles.filter(role => role !== beast && role !== guildRole)
-        const rolesToAdd = [beast, guildRole]
+        const rolesToAdd = [beast, guildRole, verifyTick]
         return { rolesToRemove, rolesToAdd }
     }
 
     if (role === "elite") {
         const rolesToRemove = roles.filter(role => role !== elite && role !== guildRole)
-        const rolesToAdd = [elite, guildRole]
+        const rolesToAdd = [elite, guildRole, verifyTick]
         return { rolesToRemove, rolesToAdd }
     }
 
     if (role === "member") {
         const rolesToRemove = roles.filter(role => role !== member && role !== guildRole)
-        const rolesToAdd = [member, guildRole]
+        const rolesToAdd = [member, guildRole, verifyTick]
         return { rolesToRemove, rolesToAdd }
     }
 
     if (role === "default") {
         const rolesToRemove = roles
-        const rolesToAdd = [defaultMember]
+        const rolesToAdd = [defaultMember, verifyTick]
         return { rolesToRemove, rolesToAdd }
     }
 
     if (role === "all") {
         const rolesToRemove = roles
+        rolesToRemove.push(verifyTick)
         rolesToRemove.push(defaultMember)
 
         return { rolesToRemove, rolesToAdd: [] }
